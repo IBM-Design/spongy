4d6b3212efc255b95ece84589328de41ae1bd853
Diego Hernandez <Diego.Hernandez@ibm.com>
2016-12-04 15:21:16 -0600

    Install IBM Design Colors package

--- js/color-picker.js
+++ js/color-picker.js
@@ -1,28 +1,51 @@
-const LOUPE_PIXELS = 9;
-const loupe = document.createDocumentFragment();
-const loupeContainer = document.createElement('div');
-const loupePixelsArray = [];
-loupeContainer.id = 'IBMEyeDropperContainer';
+const EYE_DROPPER = document.createDocumentFragment();
 
+const Loupe = {
+  // Size of number of pixels to show in Loupe.
+  SIZE: 11,
+  element: document.createElement('div'),
+  pixels: [],
+  PREFIX: 'IBMEyeDropper',
+  init: function() {
+    this.createElement();
+  },
+  render: function () {
+    EYE_DROPPER.append(this.element);
+  },
+  createElement: function() {
+    const {element, PREFIX, createPixels} = this;
+    this.element.id = `${this.PREFIX}Container`;
+    
+    // Create pixels to fill in container element.
+    this.createPixels();
+  },
+  createPixels: function() {
+    const {SIZE, PREFIX, pixels, element} = this;
+    for (let pixelIndex = 0; pixelIndex < SIZE ** 2; pixelIndex++) {
+      const pixel = document.createElement('div');
+      pixel.classList.add(`${PREFIX}Pixel`);
+      pixel.id = `${PREFIX}Pixel-${pixelIndex};`
+      pixel.style.width = `${100 / SIZE}%`;
+      pixel.style.height = `${100 / SIZE}%`;
+      // Make every new pixel lower in z-index so that the pixel divider shadows will show.
+      pixel.style.zIndex = SIZE - pixelIndex + 1;
+
+      // Cache pixels to recolor later.
+      pixels.push(pixel);
+
+      // Add to container element.
+      element.append(pixel);
+    }
+  }
+};
 // Create pixels
-for (let pixelIndex = 0; pixelIndex < LOUPE_PIXELS ** 2; pixelIndex++) {
-  const pixel = document.createElement('div');
-  pixel.classList.add('IBMEyeDropperPixel');
-  pixel.id = `IBMEyeDropperPixel-${pixelIndex};`
-  pixel.style.width = `${100 / LOUPE_PIXELS}%`;
-  pixel.style.height = `${100 / LOUPE_PIXELS}%`;
-  pixel.style.zIndex = LOUPE_PIXELS - pixelIndex + 1;
 
-  loupePixelsArray.push(pixel);
-  loupeContainer.append(pixel);
-}
-
+const ColorBox = {
+  element: document.createElement('div');
+};
 const colorBox = document.createElement('div');
 colorBox.id = 'IBMEyeDropperColorbox';
-
-
-loupe.append(loupeContainer);
-loupe.append(colorBox);
+EYE_DROPPER.append(colorBox);
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.type = 'SCREENSHOT') {
@@ -44,22 +67,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         const {body} = document;
         body.style.cursor = 'crosshair';
         body.style.pointerEvents = 'none';
-        body.append(loupe);
+        body.append(EYE_DROPPER);
       }
       const {clientX, clientY} = event;
       const [x, y] = [clientX, clientY];
-      const color = context.getImageData((x - LOUPE_PIXELS / 2) * 2, (y - LOUPE_PIXELS / 2) * 2, LOUPE_PIXELS, LOUPE_PIXELS).data;
+      const color = context.getImageData((x * 2) - Math.round(this.SIZE / 2), (y * 2) - Math.round(this.SIZE / 2), this.SIZE, this.SIZE).data;
       loupeContainer.style.transform = `translate(${x + 4}px, ${y + 4}px)`;
 
 
-      for (let pixelIndex = 0; pixelIndex < LOUPE_PIXELS ** 2; pixelIndex++) {
+      for (let pixelIndex = 0; pixelIndex < this.SIZE ** 2; pixelIndex++) {
         const colorDataBaseIndex = pixelIndex * 4;
         loupePixelsArray[pixelIndex].style.backgroundColor = `rgba(${color[colorDataBaseIndex]}, ${color[colorDataBaseIndex + 1]}, ${color[colorDataBaseIndex + 2]}, 1)`;
       }
     });
 
     document.addEventListener('click', function(event) {
-      const pixelIndex = Math.round((LOUPE_PIXELS ** 2 - 1) / 2);
+      const pixelIndex = Math.round((this.SIZE ** 2 - 1) / 2);
       colorBox.style.backgroundColor = loupePixelsArray[pixelIndex].style.backgroundColor;
     });
   }
